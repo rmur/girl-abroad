109407dc2142c608d10593ba7caf4c59f3b2f30f
rmur <romuald.murzakov@gmail.com>
2018-05-02 23:07:50 +0100

    initial commit - blog engine created

---
+++ BlogSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace RamintasBlog
+{
+    public class BlogSettings
+    {
+        public string Name { get; set; } = "RamintasBlog";
+        public string Description { get; set; } = "A short description of the blog";
+        public string Owner { get; set; } = "The Owner";
+        public int PostsPerPage { get; set; } = 2;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
